75232fa5aba207469a437b204035d30e74c97f8b
Jaak Peldes <jaakpeldes@gmail.com>
2020-03-12 09:18:26 +0800

    chip css

--- src/components/RepoChip.vue
+++ src/components/RepoChip.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-chip v-bind:color="color">
+  <v-chip v-bind:color="color" class="mr-2" small>
     <v-icon>{{ icon }}</v-icon>
     <span class="font-weight-medium">{{ count }}</span>
   </v-chip>
